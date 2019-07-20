65437b3df63751161a1e0578f601a717fa4c8e14
Ultrman82 <exelcior99@gmail.com>
2019-07-19 20:01:32 -0700

    Paginatinon done

--- src/components/Post.js
+++ src/components/Post.js
@@ -10,35 +10,48 @@ import { baseUrl } from "../actions/baseUrl";
 class Posts extends Component {
   constructor(props) {
     super(props);
-    this.state = { current_page:1 };
-    //this.RenderPage = this.RenderPage.bind(this);
+    this.state = { 
+      current_page:1,
+      total: 0,
+      posts: []   
+     };    
   }
 
+  fetchPosts = page => {
+    fetch(baseUrl + "pictures/" + page)
+    .then(res => res.json())
+    .then(posts =>      
+      {
+        this.setState({
+        posts: posts.pictures,
+        total: posts.count,      
+        current_page: page,
+      });
+      console.log(posts);
+    }      
+    );
+  }
   /* fetchPosts = async page => {
     let response = await fetch(baseUrl + "pictures/" + page);
     const data = await response.json();
 
     this.setState({
       posts: data.pictures,
-      total: data.total,
-      per_page: data.per_page,
+      total: data.count,      
       current_page: page,
     });
-  } */
-  
+  }
+   */
   componentWillMount() {
-    this.props.fetchPosts(this.state.current_page);        
+    //this.props.fetchPosts(this.state.current_page);        
+    this.fetchPosts(1);    
   }  
 
   /* componentWillReceiveProps(nextProps) {
     if (nextProps.newPost) {
       this.props.posts.unshift(nextProps.newPost);
     }
-  } */
-  shouldComponentUpdate(nextProps) {    
-    const differentDone = this.props.posts !== nextProps.posts
-    return differentDone;
-}
+  } */  
 
   RenderDel(username, id) {
     if (username === localStorage.username) {
@@ -67,22 +80,21 @@ class Posts extends Component {
   } */
   
   render() {  
-    console.log("this.props.total: " + this.props.total);
+    console.log("this.props.total: " + this.state.total);
     let pageNumbers = [];
-    for(let i = 1; i <= this.props.total/5 + 1; i++)    
+    for(let i = 1; i <= this.state.total/5 + 1; i++)    
       pageNumbers.push(i);
     const renderPage = pageNumbers.map(number => {
       let classes = this.state.current_page === number ? styles.active : '';    
       return (        
-        <span key={number} className={classes} onClick={() => {
-          this.setState({current_page:number});
+        <span key={number} className={classes} onClick={() => {          
           console.log(number);
-          this.props.fetchPosts(this.state.current_page);             
+          this.fetchPosts(number);             
         }}>{number}</span>
       );  
-      });
-    //console.log(this.props.count);    
-    const postItems = this.props.posts.map(post => (      
+      });    
+    //const postItems = this.props.posts.map(post => (      
+    const postItems = this.state.posts.map(post => (      
       <div key={post.id} className="col-md-8 col-md-5 m-1">        
       <Card>
         <CardImg top src={post.image} alt={post.username} />                 
